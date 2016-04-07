89568775b05f57544d9aa9b69229c237fc8b0f96
drkwolf <drkwolf@gmail.com>
2016-04-07 12:43:33 +0200

    small process

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $('#identifier-search').typeahead({
-  source: people,
+  source: artists,
   updater: function(selected) {
     document.location = '#/' + selected;
     console.log('Loading person:' + selected);
@@ -24,31 +24,3 @@ function hashUrlHandler() {
   }
 }
 
-function loadPerson(identifier) {
-  //show loading animation
-  $('#main').html('<p><i class="glyphicon glyphicon-refresh glyphicon-spin"></i></p>');
-
-  var resource = 'http://eis.iai.uni-bonn.de/' + identifier;
-  var source = 'people.n3';
-  //var source = 'area.nt';
-
-  var store = new rdf.LdpStore();
-  // prepare visualizer templates for uduvudu
-  $('#templates').load('templates.html');
-
-  // load triples
-  store.graph(source, function(graph, error) {
-    if (error == null) {
-      console.debug('successfully loaded ' + graph.toArray().length + 'triples');
-      // resource (entry for template search) is same as source in this example
-      uduvudu.process(graph, {
-        'resource': resource
-      }, function(out) {
-        // write the result of the processing in the main div
-        $('#main').html(out);
-      });
-    } else {
-      $('#main').html('<div class="alert alert-error" role="alert">Error: ' + error + '</div>');
-    };
-  });
-}
--- js/matcher.js
+++ js/matcher.js
@@ -1,14 +1,8 @@
 var combineMatchers = [{
-  matcherName: "personCard",
-  abstractTemplate: "personCard",
-  combineIds: ["depiction", "homepage", "mbox", "phone", "room"],
+  matcherName: "artistRelease",
+  abstractTemplate: "artistRelease",
+  combineIds: ["artistName", "releaseTitle", "releaseDate", "releasePlace"],
   order: 21000
-}, {
-  matcherName: "projectCombine",
-  templateVariable: "project",
-  abstractTemplate: "project",
-  combineIds: ["rdfschema_label", "homepage", "abstract", "rdfschema_logo"],
-  order: 200
 }];
 
 var linkMatchers = [{
@@ -21,75 +15,28 @@ var linkMatchers = [{
 }];
 
 var predicateMatchers = [{
-  matcherName: "abstract",
-  predicate: "http://purl.org/dc/terms/abstract",
-  templateVariable: "abstract",
-  abstractTemplate: "abstract",
-  order: 1100000
-}, {
-  matcherName: "projects",
-  predicate: "http://eis.iai.uni-bonn.de/Projects/",
-  templateVariable: "project",
-  abstractTemplate: "project",
-  order: 1100000
-}, {
-  matcherName: "rdfschema_logo",
-  predicate: "http://xmlns.com/foaf/0.1/logo",
-  templateVariable: "logo_url",
+  matcherName: "releaseDate",
+  predicate: "http://purl.org/dc/elements/1.1/date",
+  templateVariable: "date",
   abstractTemplate: "void",
-  order: 100
-}, {
-  matcherName: "rdfschema_label",
-  predicate: "http://www.w3.org/2000/01/rdf-schema#label",
-  templateVariable: "title",
-  abstractTemplate: "void",
-  order: 100
-}, {
-  matcherName: "foaf_name",
+  order: 1100000
+},{
+  matcherName: "name",
   predicate: "http://xmlns.com/foaf/0.1/name",
+  templateVariable: "artist_name",
+  abstractTemplate: "name",
+  order: 90000
+}, {
+  matcherName: "releaseTitle",
+  predicate: "http://xmlns.com/foaf/0.1/made",
   templateVariable: "title",
   abstractTemplate: "title",
-  order: 100000
-}, {
-  matcherName: "homepage",
-  predicate: "http://xmlns.com/foaf/0.1/homepage",
-  templateVariable: "website",
-  abstractTemplate: "void",
-  order: 20000
-}, {
-  matcherName: "depiction",
-  predicate: "http://xmlns.com/foaf/0.1/depiction",
-  templateVariable: "img_url",
-  abstractTemplate: "img",
   order: 90000
 }, {
-  matcherName: "mbox",
-  predicate: "http://xmlns.com/foaf/0.1/mbox",
-  templateVariable: "email",
+  matcherName: "releasePlace",
+  predicate: "http://purl.org/NET/c4dm/event.owl#place",
+  templateVariable: "place",
   abstractTemplate: "void",
   order: 90000
-}, {
-  matcherName: "phone",
-  predicate: "http://xmlns.com/foaf/0.1/phone",
-  templateVariable: "phone",
-  abstractTemplate: "phone",
-  order: 90000
-}, {
-  matcherName: "room",
-  predicate: "http://eis.iai.uni-bonn.de/schema/room",
-  templateVariable: "address",
-  abstractTemplate: "address",
-  order: 90000
-}, {
-  matcherName: "content",
-  predicate: "http://ns.ontowiki.net/SysOnt/Site/content",
-  templateVariable: "markdown",
-  abstractTemplate: "markdown",
-  order: 100
-}, {
-  matcherName: "description",
-  predicate: "http://purl.org/dc/terms/description",
-  templateVariable: "markdown",
-  abstractTemplate: "markdown",
-  order: 100
-}];
+}
+];

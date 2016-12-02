7feea12310eaedf8fa1a7a554c6977b30fe7f19b
drkwolf <drkwolf@gmail.com>
2016-12-02 18:11:27 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -1,26 +1,219 @@
-$('#identifier-search').typeahead({
-  source: artists,
-  updater: function(selected) {
-    document.location = '#/' + selected;
-    console.log('Loading person:' + selected);
+/**
+ * @type {String} : endpoint url
+ */
+var endPointUrl = 'https://dbpedia.org/sparql';
+
+/**
+ * @type {String} :  graph uri
+ */
+var uri         = 'http://dbpedia.org';
+
+/**
+ * helper function :
+ * add escape to string, so it can be used in RegExpression
+ * @param String str
+ * @return String
+ */
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+/**
+ * helper function : 
+ * format miliseconds into readbale format h:m:s
+ * @param duration :  integer 
+ * @return String
+ */
+function msToTime(duration) {
+  var //milliseconds = parseInt((duration%1000)/100),
+     seconds = parseInt((duration/1000)%60)
+    , minutes = parseInt((duration/(1000*60))%60)
+    , hours = parseInt((duration/(1000*60*60))%24);
+
+  var time= "";
+  if(hours > 9) {
+    time = hours + ':';
+  } else if (hours > 0) {
+    time = "0" + hours +":";
   }
-});
 
-$('form').submit(function(event) {
-  event.preventDefault();
-});
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-$(window).on('hashchange', hashUrlHandler);
-$(document).ready(hashUrlHandler);
+  return time  + minutes + ":" + seconds
+}
 
+function getArtistHint2(name, process) {
+  query = search_hint.replace(/{artist_reg}/g, name);
+  request = get_request(query, false,'application/rdf+json');
+  return $.get(request, function( data ) {
+    artists = data ["http://linkedbrainz.org/hints"]["http://xmlns.com/foaf/0.1/name"]; 
+    arr = $.map(artists, function(value, key){
+      return  [value['value']];
+    });
+    process(arr);
+  })
+}
+var getArtistHint = function(q, syncResults, asyncResults) {
+    $.ajax(
+      "http://lookup.dbpedia.org/api/search/KeywordSearch?QueryClass=artist&MaxHits=15&QueryString="+q,
+      {dataType: "json"}
+    ).done(function(data) {
+      arr = _.sortBy(
+        _.sortBy(data.results, function(b){
+          return b.label.length
+        }),function(s){
+          return -s.label.search(new RegExp('^'+q,'i'));
+        });
+
+        asyncResults(arr)
+    });
+}
+
+/**
+ * reset page content
+ */
+function reset_page() {
+      $('#q1').html('');
+      $('#q2').html('');
+      $('#q5').html('');
+      $('[data-tab=q1]').trigger('click')
+}
+
+/**
+ * load artist from url
+ */
 function hashUrlHandler() {
   if (location.hash) {
-    var identifier = location.hash.slice(2);
-    loadPerson(identifier);
+    var identifier =decodeURIComponent( location.hash.slice(2));
+      console.log(identifier);
+//    loadArtist(identifier, q1, $('#q1'), false);
+      //$('#q1').addClass('active').siblings('[data-tab]').removeClass('active');
+      //$('[data-tab=q1]').trigger('click')
+      reset_page();
     $('#identifier-search').val(identifier);
-    document.title = identifier + ' | Uduvudu Demo';
+    document.title = identifier + ' | Uduvudu LinkedBrainz';
   } else {
-
+      console.debug('hashUrlHandler: error');
   }
+}
+
+
+/**
+ * generate request url, the url is composed the following query strings :
+ *   endpoint : server url
+ *    default-graph-uri : (query with Service must not have this string)
+ *    query: sparql qurey
+ *    format: response format, it depends on the type of query see server *    documentation
+ * endpointUrl?default-graph-uri={uri}&query={query}&format={format}
+ *
+ * @param String query: sparql query
+ * @param String [format='text/turtle']: response format 
+ * @param Boolean [isUriEnabled=true]: add/remove default-graph-uri
+ *
+ * @return String
+ */
+function get_request(query, service, format) {
+ if(!service) uri = '';
+  var format = format || 'text/turtle';
+  //        var format = 'application/rdf+xml';
+  //        var format = 'text/n3';
+  //        var format = 'application/x-json+ld';
+
+  return endPointUrl+'?default-graph-uri='+encodeURIComponent(uri) +
+    '&query='+encodeURIComponent(query) +
+    '&format='+encodeURIComponent(format);
+}
+
+
+function loadArtist(name, query, elem, service) {
+  if (service === undefined) { service = false; }
+
+    var formated_name = name.replace(/ /g,"_")
+  var resource = 'http://dbpedia.org/resource/' + formated_name;
+
+  //create rdf store
+  var store = new rdf.LdpStore();
+  // format query
+  query = query.replace(/{artist}/g, decodeURIComponent(name));
+  query = query.replace(/{artist_reg}/g, decodeURIComponent(name));
+
+  // get sparql url request 
+  var request = get_request(query, service);
+
+  // load template into the main page
+  $('#templates').load('templates.html');
+
+ var promise = new Promise(function(resolve, reject) {
+   //execute request and process the result trought the callback
+   $('body').addClass('loading')
+  store.graph(request, function(graph, error) {
+
+    if (error == null) {
+      uduvudu.process(graph, {
+        resource: resource
+      }, function(out) {
+        // write the result of the processing in the main div
+        if(graph.length === 0) {
+          elem.html('<p> Noting Found </p>')
+        } else {
+          elem.html(out);
+        }
+        resolve(true); //free the promise
+      });
+    } else {
+//      $('#main').html('<div class="alert alert-error" role="alert">Error: ' + error + '</div>');
+    }
+    $('body').removeClass('loading');
+  });
+});
+  return promise;
+
+} //end loadArtist
+
+/**
+ * display query3 as modal dialog
+ */
+function loadQ3(event, ele) {
+  var identifier = decodeURIComponent(location.hash.slice(2));
+  var link  = $(ele).attr('link');
+  var q = q3_track.replace(/{release}/g, link)  ;
+//   $('[data-tab=q3]').parent().addClass('active').siblings().removeClass('active')
+   p = loadArtist(identifier, q, $('#q3'));
+   p.then(function(finished){
+   $('#release-modal-title').text($(ele).text());
+
+     $('[role=duration]').each(function () {
+       duration =msToTime($(this).text());
+       $(this).text(duration);
+     });
+     $('#release-modal').modal('show');
+
+
+   });
+         console.log($(ele).text());
+}
+
+function get_count(query, artist) {
+
+ query = query.replace(/{artist}/g, artist);
+ var request = get_request(query, false);
+ var store = new rdf.LdpStore();
+ var resource = 'http://linkedbrainz.org/_ArtistName';
+ var total = 0;
+
+ var promise = new Promise(function(resolve, reject) {
+   store.graph(request, function(graph, error) {
+     if (error == null) {
+       console.debug('successfully loaded ' + graph.toArray().length + ' triples');
+       console.log(graph.toArray());
+       total = graph.toArray()[3].object.valueOf();
+       resolve(total);
+     } else {
+       //TODO showErro
+     }
+   });
+ });
+  return promise;
 }
 
--- js/matcher.js
+++ js/matcher.js
@@ -1,42 +1,196 @@
 var combineMatchers = [{
-  matcherName: "artistRelease",
-  abstractTemplate: "artistRelease",
-  combineIds: ["artistName", "releaseTitle", "releaseDate", "releasePlace"],
+  matcherName: "releaseInfo",
+  templateVariable: "release",
+  abstractTemplate: "release",
+  combineIds: ["title", "date", "place","link"],
   order: 21000
-}];
+},{
+  matcherName: "membersInfo",
+  templateVariable: "artist",
+  abstractTemplate: "artist",
+  combineIds: ["name", "gender"],
+  order: 21000
+},{
+  matcherName: "tracksInfo",
+  templateVariable: "track",
+  abstractTemplate: "track",
+  combineIds: ["title", "duration"],
+  order: 21000
+},{
+  matcherName: "artistInfo",
+  templateVariable: "artistInfo",
+  abstractTemplate: "artistInfo",
+  combineIds: ["artistType", "gender", "ArtistWiki"],
+  order: 82000
+},{
+  matcherName: "resultsInfo",
+  abstractTemplate: "resultsInfo",
+  combineIds: ["name"],
+  order: 82000
+},{
+  matcherName: "seeAlsoInfo",
+  templateVariable: "seeAlso",
+  abstractTemplate: "seeAlso",
+  combineIds: ["seeAlso", "comment"],
+  order: 82000
+}
+];
 
-var linkMatchers = [{
-  matcherName: "projectLink",
-  predicate: "http://xmlns.com/foaf/0.1/currentProject",
-  templateVariable: "projectLink",
-  abstractTemplate: "projectLink",
-  order: 20000,
-  linkIds: ["projectCombine"]
-}];
+var linkMatchers = [{ 
+  matcherName: "releaseLink",
+  predicate: "http://linkedbrainz.org/release",
+  templateVariable: "releaseLink",
+  abstractTemplate: "releaseLink",
+  order: 22000,
+  linkIds: ["releaseInfo"]
+},{ 
+  matcherName: "membersLink",
+  predicate: "http://linkedbrainz.org/members",
+  templateVariable: "membersLink",
+  abstractTemplate: "membersLink",
+  order: 22000,
+  linkIds: ["membersInfo"]
+},{ 
+  matcherName: "tracksLink",
+  predicate: "http://linkedbrainz.org/track",
+  templateVariable: "tracksLink",
+  abstractTemplate: "tracksLink",
+  order: 22000,
+  linkIds: ["tracksInfo"]
+},{ 
+  matcherName: "resultsLink",
+  predicate: "http://linkedbrainz.org/results",
+  templateVariable: "resultsLink",
+  abstractTemplate: "resultsLink",
+  order: 22000,
+  linkIds: ["resultsInfo"]
+},{ 
+  matcherName: "seeAlsoLink",
+  predicate: "http://linkedbrainz.org/seeAlso",
+  templateVariable: "seeAlsoLink",
+  abstractTemplate: "seeAlsoLink",
+  order: 22000,
+  linkIds: ["seeAlsoInfo"]
+}
+];
+
 
 var predicateMatchers = [{
-  matcherName: "releaseDate",
+  matcherName: "date",
   predicate: "http://purl.org/dc/elements/1.1/date",
   templateVariable: "date",
-  abstractTemplate: "void",
-  order: 1100000
-},{
-  matcherName: "name",
-  predicate: "http://xmlns.com/foaf/0.1/name",
-  templateVariable: "artist_name",
-  abstractTemplate: "name",
+  abstractTemplate: "date",
   order: 90000
+},{
+  matcherName: "label",
+  predicate: "http://www.w3.org/2000/01/rdf-schema#label",
+  templateVariable: "label",
+  abstractTemplate: "label",
+  order: 90003
 }, {
-  matcherName: "releaseTitle",
-  predicate: "http://xmlns.com/foaf/0.1/made",
+  matcherName: "title",
+  predicate: "http://purl.org/dc/elements/1.1/title",
   templateVariable: "title",
   abstractTemplate: "title",
   order: 90000
 }, {
-  matcherName: "releasePlace",
+  matcherName: "place",
   predicate: "http://purl.org/NET/c4dm/event.owl#place",
   templateVariable: "place",
-  abstractTemplate: "void",
+  abstractTemplate: "place",
   order: 90000
-}
+},/* {
+  matcherName: "ArtistType",
+  predicate: "http://linkedbrainz.org/ArtistType",
+  templateVariable: "artistType",
+  abstractTemplate: "artistType",
+  order: 10000
+}, */ {
+  matcherName: "link",
+  predicate: "http://linkedbrainz.org/link",
+  templateVariable: "link",
+  abstractTemplate: "link",
+  order: 90000
+}, {
+  matcherName: "duration",
+  predicate: "http://purl.org/ontology/mo/duration",
+  templateVariable: "duration",
+  abstractTemplate: "duration",
+  order: 90000
+},{
+  matcherName: "TypeGroup",
+  predicate: "http://purl.org/ontology/mo/MusicGroup",
+  templateVariable: "TypeGroup",
+  abstractTemplate: "TypeGroup",
+  order: 10000
+},/*{
+  matcherName: "ArtistWiki",
+  predicate: "http://xmlns.com/foaf/0.1/isPrimaryTopicOf" ,
+  templateVariable: "ArtistWiki",
+  abstractTemplate: "ArtistWiki",
+  order: 10000
+}, */{ //q2,
+  matcherName: "gender",
+  predicate: "http://xmlns.com/foaf/0.1/gender" ,
+  templateVariable: "gender",
+  abstractTemplate: "gender",
+  order: 90000
+}, {
+  matcherName: "releases",
+  predicate: "http://musicbrainz.org/release",
+  templateVariable: "release",
+  abstractTemplate: "release",
+  order: 1100000
+} , {
+  matcherName: "artists",
+  predicate: "http://musicbrainz.org/artist",
+  templateVariable: "artist",
+  abstractTemplate: "artist",
+  order: 1100000
+},  {
+  matcherName: "tracks",
+  predicate: "http://musicbrainz.org/track",
+  templateVariable: "track",
+  abstractTemplate: "track",
+  order: 1100000
+}, {
+  matcherName: "abstract",
+  predicate: "http://dbpedia.org/ontology/abstract",
+  templateVariable: "abstract",
+  abstractTemplate: "abstract",
+  order: 90001
+}, {
+  matcherName: "thumbnail",
+  predicate: "http://dbpedia.org/ontology/thumbnail",
+  templateVariable: "thumbnail",
+  abstractTemplate: "thumbnail",
+  order: 90002
+}, {
+  matcherName: "name",
+  predicate: "http://xmlns.com/foaf/0.1/name",
+  templateVariable: "name",
+  abstractTemplate: "name",
+  order: 90000
+}, {
+  matcherName: "artistname",
+  predicate: "http://linkedbrainz.org/artistName",
+  templateVariable: "artistname",
+  abstractTemplate: "artistname",
+  order: 91000
+},{
+  matcherName: "seeAlso",
+  predicate: "http://www.w3.org/2000/01/rdf-schema#seeAlso",
+  templateVariable: "seeAlso",
+  abstractTemplate: "seeAlso",
+  order: 90000
+},{
+  matcherName: "comment",
+  predicate: "http://www.w3.org/2000/01/rdf-schema#comment",
+  templateVariable: "comment",
+  abstractTemplate: "comment",
+  order: 90000
+},
+
 ];
+
+

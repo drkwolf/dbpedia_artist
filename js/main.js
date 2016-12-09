f35a00c2a4c76947e0ccf4027254a4a830d03ce3
drkwolf <drkwolf@gmail.com>
2016-12-09 15:40:52 +0100

    cleanup the code

--- js/main.js
+++ js/main.js
@@ -18,42 +18,6 @@ function escapeRegExp(str) {
   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 }
 
-/**
- * helper function : 
- * format miliseconds into readbale format h:m:s
- * @param duration :  integer 
- * @return String
- */
-function msToTime(duration) {
-  var //milliseconds = parseInt((duration%1000)/100),
-     seconds = parseInt((duration/1000)%60)
-    , minutes = parseInt((duration/(1000*60))%60)
-    , hours = parseInt((duration/(1000*60*60))%24);
-
-  var time= "";
-  if(hours > 9) {
-    time = hours + ':';
-  } else if (hours > 0) {
-    time = "0" + hours +":";
-  }
-
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-  return time  + minutes + ":" + seconds
-}
-
-function getArtistHint2(name, process) {
-  query = search_hint.replace(/{artist_reg}/g, name);
-  request = get_request(query, false,'application/rdf+json');
-  return $.get(request, function( data ) {
-    artists = data ["http://linkedbrainz.org/hints"]["http://xmlns.com/foaf/0.1/name"]; 
-    arr = $.map(artists, function(value, key){
-      return  [value['value']];
-    });
-    process(arr);
-  })
-}
 var getArtistHint = function(q, syncResults, asyncResults) {
     $.ajax(
       "http://lookup.dbpedia.org/api/search/KeywordSearch?QueryClass=artist&MaxHits=15&QueryString="+q,
@@ -75,8 +39,6 @@ var getArtistHint = function(q, syncResults, asyncResults) {
  */
 function reset_page() {
       $('#q1').html('');
-      $('#q2').html('');
-      $('#q5').html('');
       $('[data-tab=q1]').trigger('click')
 }
 
@@ -92,7 +54,7 @@ function hashUrlHandler() {
       //$('[data-tab=q1]').trigger('click')
       reset_page();
     $('#identifier-search').val(identifier);
-    document.title = identifier + ' | Uduvudu LinkedBrainz';
+    document.title = identifier + ' | DBpeida  Search';
   } else {
       console.debug('hashUrlHandler: error');
   }
@@ -113,8 +75,7 @@ function hashUrlHandler() {
  *
  * @return String
  */
-function get_request(query, service, format) {
- if(!service) uri = '';
+function get_request(query format) {
   var format = format || 'text/turtle';
   //        var format = 'application/rdf+xml';
   //        var format = 'text/n3';
@@ -126,93 +87,40 @@ function get_request(query, service, format) {
 }
 
 
-function loadArtist(name, query, elem, service) {
-  if (service === undefined) { service = false; }
+function loadArtist(name, query, elem) {
 
-    var formated_name = name.replace(/ /g,"_")
-  var resource = 'http://dbpedia.org/resource/' + formated_name;
+    var formated_name = name.replace(/ /g,"_"); // dbpeida need it
+    var resource = 'http://dbpedia.org/resource/' + formated_name; // uduvudu need this to match result
 
-  //create rdf store
-  var store = new rdf.LdpStore();
-  // format query
-  query = query.replace(/{artist}/g, decodeURIComponent(name));
+    var store = new rdf.LdpStore(); //create rdf store
+    query = query.replace(/{artist}/g, decodeURIComponent(name)); // format query
+    var request = get_request(query); // get sparql url request
 
-  // get sparql url request 
-  var request = get_request(query, service);
+    $('#templates').load('templates.html'); // load template
 
-  // load template into the main page
-  $('#templates').load('templates.html');
+    var promise = new Promise(function(resolve, reject) {
+        //execute request and process the result trought the callback
+        $('body').addClass('loading')
+        store.graph(request, function(graph, error) {
 
- var promise = new Promise(function(resolve, reject) {
-   //execute request and process the result trought the callback
-   $('body').addClass('loading')
-  store.graph(request, function(graph, error) {
-
-    if (error == null) {
-      uduvudu.process(graph, {
-        resource: resource
-      }, function(out) {
-        // write the result of the processing in the main div
-        if(graph.length === 0) {
-          elem.html('<p> Noting Found </p>')
-        } else {
-          elem.html(out);
-        }
-        resolve(true); //free the promise
-      });
-    } else {
-//      $('#main').html('<div class="alert alert-error" role="alert">Error: ' + error + '</div>');
-    }
-    $('body').removeClass('loading');
-  });
-});
-  return promise;
+            if (error == null) {
+                uduvudu.process(graph, {
+                    resource: resource
+                }, function(out) {
+                    // write the result of the processing in the main div
+                    if(graph.length === 0) {
+                        elem.html('<p> Noting Found </p>')
+                    } else {
+                        elem.html(out);
+                    }
+                    resolve(true); //free the promise
+                });
+            } else {
+                // $('#main').html('<div class="alert alert-error" role="alert">Error: ' + error + '</div>');
+            }
+            $('body').removeClass('loading');
+        });
+    });
+    return promise;
 
 } //end loadArtist
-
-/**
- * display query3 as modal dialog
- */
-function loadQ3(event, ele) {
-  var identifier = decodeURIComponent(location.hash.slice(2));
-  var link  = $(ele).attr('link');
-  var q = q3_track.replace(/{release}/g, link)  ;
-//   $('[data-tab=q3]').parent().addClass('active').siblings().removeClass('active')
-   p = loadArtist(identifier, q, $('#q3'));
-   p.then(function(finished){
-   $('#release-modal-title').text($(ele).text());
-
-     $('[role=duration]').each(function () {
-       duration =msToTime($(this).text());
-       $(this).text(duration);
-     });
-     $('#release-modal').modal('show');
-
-
-   });
-         console.log($(ele).text());
-}
-
-function get_count(query, artist) {
-
- query = query.replace(/{artist}/g, artist);
- var request = get_request(query, false);
- var store = new rdf.LdpStore();
- var resource = 'http://linkedbrainz.org/_ArtistName';
- var total = 0;
-
- var promise = new Promise(function(resolve, reject) {
-   store.graph(request, function(graph, error) {
-     if (error == null) {
-       console.debug('successfully loaded ' + graph.toArray().length + ' triples');
-       console.log(graph.toArray());
-       total = graph.toArray()[3].object.valueOf();
-       resolve(total);
-     } else {
-       //TODO showErro
-     }
-   });
- });
-  return promise;
-}
-
--- js/queries.js
+++ js/queries.js
@@ -1,25 +1,17 @@
 var prefixes =`
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX dct: <http://purl.org/dc/terms/>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX dc: <http://purl.org/dc/elements/1.1/> 
-PREFIX mo: <http://purl.org/ontology/mo/>
-PREFIX event: <http://purl.org/NET/c4dm/event.owl#> 
-PREFIX lkb: <http://linkedbrainz.org/>
 PREFIX dbpedia: <http://dbpedia.org/resource/>
 PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
-PREFIX ov: <http://open.vocab.org/terms/>
 `;
 var query = prefixes +`
 construct {
-?artist a dbpedia-owl:MusicalArtist;
+?artist a dbpedia-owl:Artist;
  dbpedia-owl:abstract ?abstract;
       dbpedia-owl:thumbnail ?thumbnail;
 rdfs:comment ?comment.   
 }
   WHERE {
-    ?artist a dbpedia-owl:MusicalArtist .
+    ?artist a dbpedia-owl:Artist .
     ?artist rdfs:label "{artist}"@en .
     ?artist dbpedia-owl:abstract ?abstract.
 Optional{
